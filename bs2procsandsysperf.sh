2849d0aa6dc81286dfcd7639f9b275c4618bbd40
Richard Deodutt <richarddeodutt@gmail.com>
2022-08-06 15:53:02 -0400

    added Color output, system mem vars, and free command

--- bs2procsandsysperf.sh
+++ bs2procsandsysperf.sh
@@ -6,5 +6,30 @@
 #Script issues go here
 
 
+#Color Output
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+#No Color Output
+NC='\033[0m'
+
+
+#Total memory for the system in kB
+TotalSystemMemorykB=$(echo $(grep MemTotal /proc/meminfo | sed 's/[^0-9]*//g'))
+
+#Total available memory for the system in kB
+TotalAvailableSystemMemorykB=$(echo $(grep MemAvailable: /proc/meminfo | sed 's/[^0-9]*//g'))
+
+#Percentage of total available memory for the system
+PercentageTotalAvailableSystemMemory=$(echo "$TotalAvailableSystemMemorykB / $TotalSystemMemorykB * 100" | bc -l | cut -c "1-4")
+
+
+
+#Show the system memory situation
+free -h
+
+echo "Available Memory: $PercentageTotalAvailableSystemMemory%"
 
 #Check the system processes
+
+
