93fcd768ef4747ce3cdd3ccf46ecb5d05cbebfd0
Richard Deodutt <richarddeodutt@gmail.com>
2022-08-06 14:41:24 -0400

    header created

---
+++ bs2procsandsysperf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Richard Deodutt
+#08/06/2022
+#This script is meant to monitor processes and system performance. It will be able to show processes using over a certain ammount of system memory and allow the user to terminate them. 
+#Script issues go here
+
+
+
+#Check the system processes
