3631b387475a5c29716904d7274ecade795b1c8f
Richard Deodutt <richarddeodutt@gmail.com>
2022-08-10 12:55:02 -0400

    Color code and multiplier set up

--- bs2procsandsysperf.sh
+++ bs2procsandsysperf.sh
@@ -17,13 +17,28 @@ Yellow='\033[0;33m'
 #No color output
 NC='\033[0m'
 
+#Color code thresholds
+#100% and below
+CodeGreen=$((100))
+#50% and below
+CodeYellow=$((50))
+#25% and below
+CodeRed=$((25))
+
 #State of the system's memory
 TotalSystemMemoryKB=0
 TotalAvailableSystemMemoryKB=0
 PercentageTotalAvailableSystemMemory=0
 PercentageTotalUsedSystemMemory=0
+ColorCode=$NC
+Multiplier=1
 
-
+#Print to the console in colored text
+ColorPrint(){
+    Text=$1
+    Color=$2
+    printf "${Color}$Text${NC}\n"
+}
 
 #Function to convert KB to a human readable format
 KBtoHumanReadable(){
@@ -52,13 +67,33 @@ KBtoHumanReadable(){
 #Function to update the state of the systems memory and store it
 UpdateSystemMemoryState(){
     #Total memory for the system in KB numbers only
-    TotalSystemMemoryKB=$(echo $(grep MemTotal /proc/meminfo | sed 's/[^0-9]*//g'))
+    #TotalSystemMemoryKB=$(echo $(grep MemTotal /proc/meminfo | sed 's/[^0-9]*//g'))
+    TotalSystemMemoryKB=100
     #Total available memory for the system in KB numbers only
-    TotalAvailableSystemMemoryKB=$(echo $(grep MemAvailable: /proc/meminfo | sed 's/[^0-9]*//g'))
+    #TotalAvailableSystemMemoryKB=$(echo $(grep MemAvailable: /proc/meminfo | sed 's/[^0-9]*//g'))
+    TotalAvailableSystemMemoryKB=70
     #Percentage of total available memory for the system rounded
     PercentageTotalAvailableSystemMemory=$(printf '%.*f\n' 1 $(echo "$TotalAvailableSystemMemoryKB / $TotalSystemMemoryKB * 100" | bc -l))
     #Percentage of total used memory for the system rounded
     PercentageTotalUsedSystemMemory=$(printf '%.*f\n' 1 $(echo "100 - $PercentageTotalAvailableSystemMemory" | bc -l))
+    #Sets color code and multiplier based on set thresholds
+    if [ $(echo $PercentageTotalAvailableSystemMemory | cut -d"." -f 1) -le $CodeRed ]; then
+        #Code red low ammount of memory free
+        ColorCode=$Red
+        Multiplier=".25"
+    elif [ $(echo $PercentageTotalAvailableSystemMemory | cut -d"." -f 1) -le $CodeYellow ]; then
+        #Code yellow mid ammount of memory free
+        ColorCode=$Yellow
+        Multiplier=".50"
+    elif [ $(echo $PercentageTotalAvailableSystemMemory | cut -d"." -f 1) -le $CodeGreen ]; then
+        #Code green high ammount of memory free
+        ColorCode=$Green
+        Multiplier="1"
+    else
+        #Code error something is wrong so use defaults
+        ColorCode=$NC
+        Multiplier="1"
+    fi
 }
 
 #Function to print/echo the last state of the systems memory stored
@@ -67,13 +102,13 @@ PrintSystemMemoryStatus(){
     echo "Total System Memory: "$(KBtoHumanReadable $TotalSystemMemoryKB)
     #Total available memory for the system in a human readable format
     echo "Total Available System Memory: "$(KBtoHumanReadable $TotalAvailableSystemMemoryKB)
-    #Percentage of total available memory for the system rounded
-    echo "Percent of Available Memory: $PercentageTotalAvailableSystemMemory %"
-    #Percentage of total used memory for the system rounded
-    echo "Percent of Used Memory: $PercentageTotalUsedSystemMemory %"
+    #Percentage of total available memory for the system rounded and color coded
+    echo "Percent of Available Memory: $(ColorPrint $PercentageTotalAvailableSystemMemory ${ColorCode}) %"
+    #Percentage of total used memory for the system rounded and color coded
+    echo "Percent of Used Memory: $(ColorPrint $PercentageTotalUsedSystemMemory ${ColorCode}) %"
 }
 
-#Check the system memory situation
+#Checking the system memory situation
 
 #Update the system memory state
 UpdateSystemMemoryState
@@ -81,7 +116,7 @@ UpdateSystemMemoryState
 #Print the system memory state
 PrintSystemMemoryStatus
 
-#Check how much memory is free and assign a code- code green means free memory is high, code yellow means free memory is mid and code red means free memory is low. depending on code it will change the threshold for showing processes to be selected for termination.
+depending on code it will change the threshold for showing processes to be selected for termination.
 
 
 
